23050e5300ea8091109925fec9d3fa230e69b000
siddhant-2002 <siddeshmukh711@gmail.com>
2024-07-20 21:14:13 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-  <div className="bg-red-500 w-full h-16"></div> {/* Horizontal div */}
-  <div className="flex justify-between w-full mt-4">
-    <div className="bg-blue-500 w-1/2 h-48"></div> {/* First vertical div */}
-    <div className="bg-green-500 w-1/2 h-48"></div> {/* Second vertical div */}
-  </div>
-</div>
+    <div>
+      
+    </div>
   )
 }
 
