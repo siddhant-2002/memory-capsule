439aa7212593bf1e9ecf8d0e56d1a8e5b307c1d6
siddhant-2002 <siddeshmukh711@gmail.com>
2024-07-20 18:59:26 +0530

    home page

---
+++ src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Profiles from '../pages/Profiles'
+
+
+const Feed = (props) => {
+    
+    return (
+        <div className="w-full md:w-[70vw] h-auto md:h-[80vh] flex flex-col">
+            
+        </div>
+    )
+}
+
+export default Feed
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = (props) => {
+    // Step 1: Define the changeHandler function
+    
+    return (
+        <div className='md:h-[8vh] md:w-[80vw]'>
+           
+        </div>
+    )
+}
+
+export default Header;
