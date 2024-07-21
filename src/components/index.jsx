b7f7ce7d1205eb4771b1f03bf081ef0733333cfe
siddhant-2002 <siddeshmukh711@gmail.com>
2024-07-21 13:56:03 +0530

    new updates

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,14 +6,13 @@ import Profile from './Profile'
 export const Main = () => {
   return (
     <div className='h-[100vh] w-[100vw] grid lg:grid-cols-8 bg-white gap-y-6 gap-x-6 p-12 grid-col'>
-        <div className='lg:col-span-8 bg-gray-400 rounded-2xl flex place-content-center'>
-            {/* <h1 className='text-4xl font-bold'>Navbar here</h1> */}
+        <div className='lg:col-span-8 bg-gray-200 rounded-2xl flex place-content-center'>
             <Navbar />
         </div>
-        <div className='lg:col-span-6 lg:h-full bg-blue-500 row-span-8 rounded-2xl flex place-content-center'>
+        <div className='lg:col-span-6 lg:h-full bg-gray-200 row-span-8 rounded-2xl flex place-content-center'>
             <Feed />
         </div>
-        <div className='lg:col-span-2 lg:h-full bg-green-500 row-span-8 rounded-2xl flex place-content-center'>
+        <div className='lg:col-span-2 lg:h-full bg-gray-200 row-span-8 rounded-2xl flex place-content-center'>
             <Profile/>
         </div>
     </div>
