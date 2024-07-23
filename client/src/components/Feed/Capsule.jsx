7b3f2e6aae4c1a110ecb813702e2e69cd69b3037
Kartik Kesbhat <kartikkeshbhat2003@gmail.com>
2024-07-23 14:10:11 +0530

    Grid upgrade

--- client/src/components/Feed/Capsule.jsx
+++ client/src/components/Feed/Capsule.jsx
@@ -20,68 +20,73 @@ export const Capsule = () => {
   }, []);
 
   return (
-    <div class="w-full h-full border border-black overflow-hidden relative">
-      <div class="w-full h-full overflow-y-scroll absolute">
-        <div class=" bg-gradient-to-b">
-          <div className='FLEX flex-col gap-3 sticky top-0 bg-gradient-to-b from-black text-white pb-8'>
-            <h1 className='text-[70px] ml-6 font-bold'>{time}</h1>
-            <p className='text-4xl font-semibold ml-6'>CAPSULE NAME</p>
-          </div>
-          <div className="flex flex-wrap justify-between">
-            <div className="w-[calc(100%/5)] h-[250px] p-2 rounded-md">
-              <img src={image1} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image2} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image3} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image4} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image1} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image2} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image3} alt="" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-[calc(100%/4)] h-[250px] p-2">
-              <img src={image4} alt="" className="w-full h-full object-cover" />
-            </div>
-          </div>
+    <div className='w-[100%] h-[100%] overflow-scroll overflow-x-hidden'>
+      <div className="w-[100%]  flex flex-col gap-3 sticky top-0 bg-gradient-to-b from-black text-white px-6 pb-8">
+        <h1 className="text-[70px] font-bold">{time}</h1>
+        <p className="text-4xl font-semibold ">CAPSULE NAME</p>
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-[calc(100%/5)] h-[300px] p-2 rounded-md">
+          <img src={image1} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image2} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image3} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image4} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image1} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image2} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image3} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-[calc(100%/3)] h-[300px] p-2">
+          <img src={image4} alt="" className="w-full h-full object-cover" />
         </div>
       </div>
     </div>
-    // <div className=''>
-    //   <div className='w-full flex flex-wrap '>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image1} alt='' className='w-full h-full object-cover' />
+
+    // <div class="w-full h-full border border-black overflow-hidden relative">
+    //   <div class="w-full h-full overflow-y-scroll absolute">
+    //     <div class=" bg-gradient-to-b">
+    //       <div className='FLEX flex-col gap-3 sticky top-0 bg-gradient-to-b from-black text-white pb-8'>
+    //         <h1 className='text-[70px] ml-6 font-bold'>{time}</h1>
+    //         <p className='text-4xl font-semibold ml-6'>CAPSULE NAME</p>
     //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image2} alt='' className='w-full h-full object-cover' />
+    //       <div className="flex flex-wrap justify-between">
+    //         <div className="w-[calc(100%/5)] h-[250px] p-2 rounded-md">
+    //           <img src={image1} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image2} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image3} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image4} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image1} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image2} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image3} alt="" className="w-full h-full object-cover" />
+    //         </div>
+    //         <div className="w-[calc(100%/4)] h-[250px] p-2">
+    //           <img src={image4} alt="" className="w-full h-full object-cover" />
+    //         </div>
     //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image3} alt='' className='w-full h-full object-cover' />
-    //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image4} alt='' className='w-full h-full object-cover' />
-    //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image5} alt='' className='w-full h-full object-cover' />
-    //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image6} alt='' className='w-full h-full object-cover' />
-    //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image2} alt='' className='w-full h-full object-cover' />
-    //       </div>
-    //       <div className='w-[calc(100%/3)] h-[300px] p-2'>
-    //         <img src={image1} alt='' className='w-full h-full object-cover' />
-    //       </div>
+    //     </div>
     //   </div>
     // </div>
   );
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -8,7 +8,7 @@ export const Main = () => {
   return (
     <div className='h-[100vh] w-[100vw] grid lg:grid-cols-8 bg-white gap-y-6 gap-x-6 p-12 grid-col'>
 
-        <div className='lg:col-span-8 bg-gray-200 rounded-2xl flex place-content-center'>
+        <div className='lg:col-span-8 bg-gray-200 row-span-2 rounded-2xl flex place-content-center'>
             <Navbar />
         </div>
         <div className='lg:col-span-6 lg:h-full bg-gray-200 row-span-8 rounded-2xl flex place-content-center overflow-y-hidden border-2'>
